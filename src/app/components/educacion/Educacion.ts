ca6bc4f3c643191ce83b407353f4f28c2359f585
Miguel <chanampamiguel@gmail.com>
2022-07-21 11:58:09 -0300

    Creación interface y mock de educacion

---
+++ src/app/components/educacion/Educacion.ts
@@ -0,0 +1,9 @@
+export interface Educacion {
+  id: number;
+  titulo: string;
+  institucion: string;
+  descripcion: string;
+  imagen: string;
+  fecha_inicio: string;
+  fecha_fin: string;
+}
---
+++ src/app/components/educacion/mock-educacion.ts
@@ -0,0 +1,22 @@
+import { Educacion } from './Educacion';
+
+export const LISTADO_EDUCACION: Educacion[] = [
+  {
+    id: 1,
+    titulo: 'Nombre carrera 1',
+    institucion: 'Universidad 1',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eos exercitationem libero, soluta reiciendis voluptates quod molestias laborum veniam minus deleniti, quia inventore iure illum voluptatibus minima amet! Recusandae, eos',
+    imagen: 'logo_university_1.jpg',
+    fecha_inicio: '2013',
+    fecha_fin: '2018'
+  }, 
+  {
+    id: 2,
+    titulo: 'Nombre carrera 2',
+    institucion: 'Universidad 2',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eos exercitationem libero, soluta reiciendis voluptates quod molestias laborum veniam minus deleniti, quia inventore iure illum voluptatibus minima amet! Recusandae, eos',
+    imagen: 'logo_university_2.jpg',
+    fecha_inicio: '2012',
+    fecha_fin: '2009'
+  }
+];
