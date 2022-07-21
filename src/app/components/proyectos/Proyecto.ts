6c767d61b32790935cf83e29c3f49b4daf12f253
Miguel <chanampamiguel@gmail.com>
2022-07-21 14:46:28 -0300

    Creación interface y mock de los proyectos

---
+++ src/app/components/proyectos/Proyecto.ts
@@ -0,0 +1,8 @@
+export interface Proyecto {
+  id: number;
+  nombre: string;
+  descrpcion: string;
+  imagen: string;
+  link_proyecto: string;
+  link_repositorio: string;
+}
---
+++ src/app/components/proyectos/mock-proyectos.ts
@@ -0,0 +1,28 @@
+import { Proyecto } from './Proyecto';
+
+export const LISTADO_PROYECTOS: Proyecto[] = [
+  {
+    id: 1,
+    nombre: 'Nombre Proyecto 1',
+    descrpcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    imagen: 'project_1.jpg',
+    link_proyecto: '#',
+    link_repositorio: '#'
+  },
+  {
+    id: 2,
+    nombre: 'Nombre Proyecto 2',
+    descrpcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    imagen: 'project_2.jpg',
+    link_proyecto: '#',
+    link_repositorio: '#'
+  },
+  {
+    id: 3,
+    nombre: 'Nombre Proyecto 3',
+    descrpcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    imagen: 'project_3.jpg',
+    link_proyecto: '#',
+    link_repositorio: '#'
+  }
+];
