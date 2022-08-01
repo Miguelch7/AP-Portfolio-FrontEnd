c7a17db105fecf4e897b0e3696e6c73e1e0ce8bd
Miguel <chanampamiguel@gmail.com>
2022-08-01 12:20:22 -0300

    Traer detalles a header desde backend

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faLinkedin, IconDefinition  } from '@fortawesome/free-brands-svg-icons';
+import { DetalleService } from '../../services/detalle.service';
 
 @Component({
   selector: 'app-header',
@@ -8,12 +9,23 @@ import { faLinkedin, IconDefinition  } from '@fortawesome/free-brands-svg-icons'
 })
 export class HeaderComponent implements OnInit {
 
+  nombre!: string;
+  apellido!: string;
+  profesion!: string;
+
   mensajes: string[] = ['el desarrollo web.', 'la programación y la tecnología.', 'trabajar en equipo.', 'aprender.'];
   faLinkedin: IconDefinition = faLinkedin;
 
-  constructor() { }
+  constructor(
+    private detalleService: DetalleService
+  ) { }
 
   ngOnInit(): void {
+    this.detalleService.getDetalle().subscribe(({ nombre, apellido, profesion }) => {      
+      this.nombre = nombre.toUpperCase();
+      this.apellido = apellido.toUpperCase();
+      this.profesion = profesion;
+    });
   }
 
 }
