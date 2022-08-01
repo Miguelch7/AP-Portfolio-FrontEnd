ee48f1c88b3219e42d1ffa03832c73481b29ce68
Miguel <chanampamiguel@gmail.com>
2022-08-01 12:15:31 -0300

    Creación interface Detalle

---
+++ src/app/components/sobre-mi/Detalle.ts
@@ -0,0 +1,10 @@
+export interface Detalle {
+  id: number;
+  nombre: string;
+  apellido: string;
+  profesion: string;
+  descripcion: string;
+  imagen: string;
+  direccion: string;
+  cv: string;
+}
