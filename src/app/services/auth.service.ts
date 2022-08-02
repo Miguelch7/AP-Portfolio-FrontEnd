957bc52d7ef712114aaeaf6ed9217f714c152f8a
Miguel <chanampamiguel@gmail.com>
2022-08-02 14:26:51 -0300

    Creacion auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl: string = environment.apiUrl + '/auth/login';
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  login(email: string, password: string): void {
+    this.http.post(this.apiUrl, { email, password }).subscribe((res: any) => {
+      // console.log(res);
+      localStorage.setItem('auth_token', res.token);
+      this.router.navigate(['']);
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('auth_token');
+  }
+
+  public get isLogged(): boolean {
+    return localStorage.getItem('auth_token') !== null;
+  }
+ }
